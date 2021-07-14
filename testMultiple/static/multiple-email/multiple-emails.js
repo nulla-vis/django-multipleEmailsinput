e847d3d35bf31f93a710ccc5b4aebdce7fd29ab2
Azhandi Usemahu <55913058+nulla-vis@users.noreply.github.com>
2021-07-14 15:48:22 +0700

    Update multiple-emails.js

--- testMultiple/static/multiple-email/multiple-emails.js
+++ testMultiple/static/multiple-email/multiple-emails.js
@@ -15,8 +15,8 @@
 		var deleteIconHTML = "";
 		if (settings.theme.toLowerCase() == "Bootstrap".toLowerCase())
 		{
-			// deleteIconHTML = '<a href="#" class="multiple_emails-close" title="Remove"><span class="glyphicon glyphicon-remove"></span></a>'; //close button use glyphon remove icon
-			deleteIconHTML = '<a href="#" class="multiple_emails-close" title="Remove"><span aria-hidden="true">&times;</span</a>'; //close button use bootstap icon
+			// deleteIconHTML = '<a href="#" class="multiple_emails-close" title="Remove"><span class="glyphicon glyphicon-remove"></span></a>'; //close button use glyphon remove icon, (uncomment this and the ctylesheet cdn in html file)
+			deleteIconHTML = '<a href="#" class="multiple_emails-close" title="Remove"><span aria-hidden="true">&times;</span</a>'; //close button use bootstap icon (comment this if use the top remove icon)
 		}
 		else if (settings.theme.toLowerCase() == "SemanticUI".toLowerCase() || settings.theme.toLowerCase() == "Semantic-UI".toLowerCase() || settings.theme.toLowerCase() == "Semantic UI".toLowerCase()) {
 			deleteIconHTML = '<a href="#" class="multiple_emails-close" title="Remove"><i class="remove icon"></i></a>';
