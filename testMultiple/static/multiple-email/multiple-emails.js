045f3777c3a90aa5b8e51632f680246e4dea17a2
aanusemahu <aanusemahu@gmail.com>
2021-07-14 15:44:57 +0700

    fix close icon

--- testMultiple/static/multiple-email/multiple-emails.js
+++ testMultiple/static/multiple-email/multiple-emails.js
@@ -19,11 +19,11 @@
 			deleteIconHTML = '<a href="#" class="multiple_emails-close" title="Remove"><span aria-hidden="true">&times;</span</a>'; //close button use bootstap icon
 		}
 		else if (settings.theme.toLowerCase() == "SemanticUI".toLowerCase() || settings.theme.toLowerCase() == "Semantic-UI".toLowerCase() || settings.theme.toLowerCase() == "Semantic UI".toLowerCase()) {
-			deleteIconHTML = '<a href="#" class="multiple_emails-close" title="Remove"><i class="remove icon"></i>b</a>';
+			deleteIconHTML = '<a href="#" class="multiple_emails-close" title="Remove"><i class="remove icon"></i></a>';
 		}
 		else if (settings.theme.toLowerCase() == "Basic".toLowerCase()) {
 			//Default which you should use if you don't use Bootstrap, SemanticUI, or other CSS frameworks
-			deleteIconHTML = '<a href="#" class="multiple_emails-close" title="Remove"><i class="basicdeleteicon">Remove</i>c</a>';
+			deleteIconHTML = '<a href="#" class="multiple_emails-close" title="Remove"><i class="basicdeleteicon">Remove</i></a>';
 		}
 		
 		return this.each(function() {
